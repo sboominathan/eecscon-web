31451e9b0b856fa22985ac77a9c8281b6187ce4e
Sooraj Boominathan <sooraj.boomi@gmail.com>
2017-01-15 16:27:06 -0500

    added application submission

--- routes/index.js
+++ routes/index.js
@@ -24,27 +24,27 @@ router.post('/signup', function(req, res, next) {
   var password = password_hash.generate(req.body.password);
 
   var post = {
-  	name: name,
-  	email: email,
-  	username: username,
-  	password: password
+    name: name,
+    email: email,
+    username: username,
+    password: password
   }
 
   connection.query("SELECT username from users where username='"+username+"'", function(err,rows){
-  	if (rows.length == 0){
-  		connection.query('INSERT INTO users SET ?', post, function (err, result) {
-	  		if (err){
-				console.log(err);
-			} else{
-				console.log("New user received.");
-			}	
-  		});
-  		res.render('index', { title: 'Express' });
-  	}
-  	else{
-  		res.render('signup', { title: 'Express' });
-  	}
-  	});
+    if (rows.length == 0){
+      connection.query('INSERT INTO users SET ?', post, function (err, result) {
+        if (err){
+        console.log(err);
+      } else{
+        console.log("New user received.");
+      } 
+      });
+      res.render('application', { title: 'Express' });
+    }
+    else{
+      res.render('signup', { title: 'Express' });
+    }
+    });
   
 });
 
@@ -61,24 +61,56 @@ router.post('/login', function(req, res, next) {
   var password = req.body.password;
 
   connection.query("SELECT * from users where username='"+username+"'", function(err,rows){
-  	if (rows.length == 1){
-  		console.log(rows[0]);
-  		var user = rows[0];
-  		if (password_hash.verify(password, user['password'])){
-  			req.session.user = user['username'];
-  			res.render('index',{ title: 'Express' });
-  		} else{
-  			res.render('signup',{ title: 'Express' });
-  		}
-  	}
-  	else{
-  		res.render('signup', { title: 'Express' });
-  	}
-  	});
+    if (rows.length == 1){
+      console.log(rows[0]);
+      var user = rows[0];
+      if (password_hash.verify(password, user['password'])){
+        req.session.user = user['username'];
+        res.render('application',{ title: 'Express' });
+      } else{
+        res.render('signup',{ title: 'Express' });
+      }
+    }
+    else{
+      res.render('signup', { title: 'Express' });
+    }
+    });
 });
 
 router.get('/application', function(req, res, next) {
-  res.render('application', { title: 'Express' });
+  if (req.session && req.session.user){
+    res.render('application', { title: 'Express' });
+  } else{
+    res.render('signup', { message: 'Login' });
+  }
 });
+
+router.post('/apply', function(req,res,next){
+
+  var post = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    year: req.body.year,
+    course:req.body.course,
+    superurop: req.body.superurop,
+    advisor: req.body.advisor,
+    presentation: req.body.presentation,
+    field: req.body.field,
+    specify: req.body.specify,
+    abstract: req.body.abstract
+  }
+
+  connection.query('INSERT INTO applications SET ?', post, function (err, result) {
+      if (err){
+        console.log(err);
+      } else {
+        console.log("new application received");
+      }
+  });
+
+  res.render("index",{});
+
+})
 
 module.exports = router;
