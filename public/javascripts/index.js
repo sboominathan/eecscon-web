3fc08764e3a51f2b5a49877be9adb2bbf938f1f5
Sooraj Boominathan <sooraj.boomi@gmail.com>
2017-02-03 01:22:33 -0500

    more CSS changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -13,16 +13,16 @@ $(document).ready(function(){
             $("#navbar-collapse").css({"background-color":"#3361FF"});     
             
         } else if (scrollPercent> 25 && scrollPercent< 41) {
-            $("nav").css({"background-color":"#ee9b2a"});
-            $("#navbar-collapse").css({"background-color":"#ee9b2a"});     
+            $("nav").css({"background-color":"rgb(246,135,18)"});
+            $("#navbar-collapse").css({"background-color":"rgb(246,135,18)"});     
             
         } else if (scrollPercent> 41 && scrollPercent< 76) {
-            $("nav").css({"background-color":"#e2616e"});
-            $("#navbar-collapse").css({"background-color":"#e2616e"}); 
+            $("nav").css({"background-color":"rgb(239,46,50)"});
+            $("#navbar-collapse").css({"background-color":"rgb(239,46,50)"}); 
 
         } else if (scrollPercent> 76 && scrollPercent< 90) {
-            $("nav").css({"background-color":"#96ceed"});
-            $("#navbar-collapse").css({"background-color":"#96ceed"});        
+            $("nav").css({"background-color":"rgb(81,173,229)"});
+            $("#navbar-collapse").css({"background-color":"rgb(81,173,229)"});        
         } else{
             $("nav").css({"background-color":"#1d1d1d"});   
             $("#navbar-collapse").css({"background-color":"#1d1d1d"});   
